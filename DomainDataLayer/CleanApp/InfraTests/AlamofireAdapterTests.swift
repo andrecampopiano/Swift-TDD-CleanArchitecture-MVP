375f4069232e350c5bfaff346ae7724dcdff4555
Andre Luis Campopiano <andrelc@ciandt.com>
2022-02-28 10:20:10 -0300

    move SUT creation to a factory helper method

--- DomainDataLayer/CleanApp/InfraTests/AlamofireAdapterTests.swift
+++ DomainDataLayer/CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -25,11 +25,7 @@ class AlamofireAdapterTests: XCTestCase {
     
     func testPostShouldMakeRequestWithValidUrlAndMethod() {
         let url = makeUrl
-        
-        let configuration = URLSessionConfiguration.default
-        configuration.protocolClasses = [UrlProtocolStub.self]
-        let session = Session(configuration: configuration)
-        let sut = AlamofireAdapter(session: session)
+        let sut = makeSut()
         sut.post(to: url, with: makeValidData)
         let exp = expectation(description: "waiting")
         UrlProtocolStub.obeserveRequest { request in
@@ -42,19 +38,23 @@ class AlamofireAdapterTests: XCTestCase {
     }
     
     func testPostShouldMakeRequestWithNoData() {
-        let url = makeUrl
-        
-        let configuration = URLSessionConfiguration.default
-        configuration.protocolClasses = [UrlProtocolStub.self]
-        let session = Session(configuration: configuration)
-        let sut = AlamofireAdapter(session: session)
-        sut.post(to: url, with: nil)
+        let sut = makeSut()
+        sut.post(to: makeUrl, with: nil)
         let exp = expectation(description: "waiting")
         UrlProtocolStub.obeserveRequest { request in
             XCTAssertNil(request.httpBodyStream)
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
+    }
+}
+
+extension AlamofireAdapterTests {
+    func makeSut() ->  AlamofireAdapter {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        return  AlamofireAdapter(session: session)
     }
 }
 
